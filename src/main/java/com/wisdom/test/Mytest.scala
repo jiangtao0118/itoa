f6313112641f84380fc90793a2a19a83103b0ee9
jiangtao0118 <1753230173@qq.com>
2018-10-16 23:29:10 +0800

    add Mytest

--- src/main/java/com/wisdom/test/Mytest.scala
+++ src/main/java/com/wisdom/test/Mytest.scala
@@ -1,19 +1,32 @@
 package com.wisdom.test
 
 
+import breeze.linalg.*
 import com.wisdom.spark.common.util.SparkContextUtil
 import org.apache.commons.csv.CSVParser
 import org.apache.hadoop.conf.Configuration
+import org.apache.spark.sql.{SQLContext, SparkSession}
+
+import scala.collection.immutable.HashMap
 
 
 object Mytest {
 
   def main(args: Array[String]): Unit = {
-    val sc = SparkContextUtil.getInstance()
-    Configuration
-    CSVParser
-    sc.textFile()
-    sc.
+
+    val spark  = SparkSession.builder.
+      master("local")
+      .appName("spark session example")
+      .getOrCreate()
+    val df = spark.read.option("header","true").csv("data.csv")
+
+
+  }
+
+  def echo( str:String*):Unit={
+    for(arg <- str){
+      print(arg)
+    }
   }
 
 }
