09cae528a8e8ae899cd03aee118a68e1ef595f88
jiangtao0118 <1753230173@qq.com>
2018-10-16 23:31:37 +0800

    update testColumn

--- src/main/java/com/wisdom/test/testColumn.java
+++ src/main/java/com/wisdom/test/testColumn.java
@@ -1,8 +1,25 @@
 package com.wisdom.test;
 
+import org.apache.spark.sql.DataFrame;
+import org.apache.spark.sql.Dataset;
+import org.apache.spark.sql.Row;
+import org.apache.spark.sql.SparkSession;
+
+import java.util.HashMap;
+
 public class testColumn {
 
     public  static void main(String[] args){
+        String path = "data.csv";
+        HashMap<String,String> map = new HashMap<String, String>();
+        map.put("header","ture");
+        map.put("seq",",");
+        map.put("path",path);
+        SparkSession spark = SparkSession.builder()
+                .master("local")
+                .appName("spark session example").getOrCreate();
+        DataFrame data =spark.read().options(map).format("csv").load();
 
+        data.show();
     }
 }
